feb6265eb1cb82ad5c719c67ba06c850dcbd6a59
Jakub Dzikowski <dzikowskijakub2001@gmail.com>
2024-03-17 13:58:42 +0100

    projectform structed

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <template>
   <div id="app">
-    <ProjectForm />
     <ProjectList />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import ProjectForm from './components/projects-components/ProjectForm.vue';
 import ProjectList from './components/projects-components/ProjectList.vue';
 
 export default defineComponent({
   components: {
-    ProjectForm,
     ProjectList
   }
 });
