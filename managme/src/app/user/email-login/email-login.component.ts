16bedbf3218b3e809db057af9e0f668ae91726cc
Jakub Dzikowski <dzikowskijakub2001@gmail.com>
2024-04-19 13:21:21 +0200

    logging/register form added(no server side logic yet)

---
+++ managme/src/app/user/email-login/email-login.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-email-login',
+  templateUrl: './email-login.component.html',
+  styleUrl: './email-login.component.scss',
+})
+export class EmailLoginComponent implements OnInit {
+  form: FormGroup;
+  type: 'login' | 'signup' | 'reset' = 'signup';
+  loading = false;
+  //serverMessage: string;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({});
+  }
+
+  //shouldn't form controls be in constructor?
+  ngOnInit() {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      passwordConfirm: ['', []],
+    });
+  }
+
+  changeType(val: any) {
+    this.type = val;
+  }
+
+  get isLogin() {
+    return this.type === 'login';
+  }
+
+  get isSignup() {
+    return this.type === 'signup';
+  }
+
+  get isPasswordReset() {
+    return this.type === 'reset';
+  }
+
+  get email() {
+    return this.form.get('email');
+  }
+  get password() {
+    return this.form.get('password');
+  }
+
+  get passwordConfirm() {
+    return this.form.get('passwordConfirm');
+  }
+
+  get passwordDoesMatch() {
+    if (this.type !== 'signup') {
+      return true;
+    } else {
+      return this.password?.value === this.passwordConfirm?.value;
+    }
+  }
+
+  async onSubmit() {
+    console.log(this.form.value, 'logging logging!');
+  }
+}
--- managme/src/app/user/user.module.ts
+++ managme/src/app/user/user.module.ts
@@ -3,14 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
 import { SharedModule } from '../shared/shared.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { EmailLoginComponent } from './email-login/email-login.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [EmailLoginComponent, LoginPageComponent],
   imports: [
     CommonModule,
     UserRoutingModule,
-    SharedModule
+    SharedModule,
+    ReactiveFormsModule
   ]
 })
 export class UserModule { }
