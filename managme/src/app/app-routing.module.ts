dacd808ec5168ef94e0435c8a6be7dd8ae8e5894
Jakub Dzikowski <dzikowskijakub2001@gmail.com>
2024-04-21 14:40:54 +0200

    authguard added for now disabled

--- managme/src/app/app-routing.module.ts
+++ managme/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
+import { AuthGuard } from './user/auth.guard';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent},
@@ -8,7 +9,8 @@ const routes: Routes = [
     path: 'login', loadChildren: () => import('./user/user.module').then(m => m.UserModule)
   },
   {
-    path: 'projects', loadChildren: () => import('./projects/projects.module').then(m => m.ProjectsModule)
+    path: 'projects', loadChildren: () => import('./projects/projects.module').then(m => m.ProjectsModule),
+    //canActivate: [AuthGuard]
   }
 ];
 
